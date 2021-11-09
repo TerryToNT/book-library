54f632e7f207c7b5e95917d7aed56e747c1a6bea
TerryToNT <54839074+TerryToNT@users.noreply.github.com>
2021-11-09 22:10:40 +0200

    Optimization of the BookLibrary contract

--- BookLibrary.sol
+++ BookLibrary.sol
@@ -5,75 +5,100 @@ import "./Ownable.sol";
 
 contract BookLibrary is Ownable {
     
-    event NewBook(string name, uint copies);
+    event NewBook(string name, uint copies, uint id);
 
     struct Book {
         string name;
         uint copies;
+        uint id;
     }
     
-    Book[] public books;
- 
-    mapping (uint => address[]) public bookToBorrowers;
+    uint bookIdDigits = 12; //lenght of book id
+    uint bookIdModulus = 10 ** bookIdDigits;
+    
+    uint[] public bookIds;
+    
     //address of borrower mapped to mapping of bookId and boolean if book is borrowed from him at the moment
     mapping (address => mapping (uint => bool)) public borrowersBooks;
+    //bookId to Book
+    mapping (uint => Book) public books;
+    //bookId to array of borrowers addresses
+    mapping (uint => address[]) public bookToArrayOfBorrowers;
+    //bookId to mapping of borrower address to boolean. Used to validate if the borrower is already added to the array of borrowers for specific book
+    mapping (uint => mapping (address => bool)) public bookToBorrowers;
+
     
-    modifier isValidBook(string memory _name) {
+    modifier isValidBookName(string memory _name) {
         bytes memory tempBookName = bytes(_name);
         require(tempBookName.length > 0, 'Invalid book name!');
         _;
     }
     
-    function addNewBook(Book calldata book) public onlyOwner isValidBook(book.name) {
-        books.push(Book(book.name, book.copies));
-        emit NewBook(book.name, book.copies);
+    modifier validateChoosenBook(uint _bookId) {
+        require(books[_bookId].copies > 0, 'There is no more copies of this book.');
+        require(!borrowersBooks[msg.sender][_bookId], 'You already borrowed this book.');
+        _;
+    }
+    
+    modifier isNotBorrowed(uint _bookId) {
+        require(borrowersBooks[msg.sender][_bookId], 'You have not borrow this book.');
+        _;
+    }
+    
+    modifier bookIsAdded(uint _bookId) {
+        require(books[_bookId].id == 0, 'This book is already added.');
+        _;
+    }
+    
+    function addNewBook(Book calldata book) public onlyOwner isValidBookName(book.name) {
+        uint bookId = _generateBookId(book.name);
+        _createBook(book.name, book.copies, bookId);
+        emit NewBook(book.name, book.copies, bookId);
     }
     
     function addCopiesToBook(uint _bookId, uint _copiesNumber) public onlyOwner {
         books[_bookId].copies += _copiesNumber;
     }
     
-    function editBookName(uint _bookId, string memory _newBookName) public onlyOwner isValidBook(_newBookName) {
-        books[_bookId].name = _newBookName;
+    
+    function showAvailableBooks() public view returns(uint[] memory) {
+        //Returns array with all books ids
+        return bookIds;
     }
     
-    function showAvailableBooks() public view returns(Book[] memory) {
-        //TODO return only available books
-        return books;
-    }
-    
-    function borrowBook(uint _bookId) public {
-        require(books[_bookId].copies > 0, 'There is no more copies of this book.');
-        require(!borrowersBooks[msg.sender][_bookId], 'You already borrowed this book.');
-
+    function borrowBook(uint _bookId) public validateChoosenBook(_bookId) {
         borrowersBooks[msg.sender][_bookId] = true;
         books[_bookId].copies--;
         _addBorrowerToBookList(_bookId);
     }
     
-    function returnBook(uint _bookId) public {
-        require(borrowersBooks[msg.sender][_bookId], 'You have not borrow this book.');
-        
+    function returnBook(uint _bookId) public isNotBorrowed(_bookId) {
         borrowersBooks[msg.sender][_bookId] = false;
         books[_bookId].copies++;
     }
     
     function showBookBorrowers(uint _bookId) public view returns(address[] memory) {
-        return bookToBorrowers[_bookId];
+        return bookToArrayOfBorrowers[_bookId];
+    }
+    
+    function getBookData(uint _bookId) public view returns(Book memory) {
+        return books[_bookId];
     }
     
     function _addBorrowerToBookList(uint _bookId) internal {
-        bool hasRecord = false;
-        
-        for(uint i = 0; i < bookToBorrowers[_bookId].length; i++) {
-            if (bookToBorrowers[_bookId][i] == msg.sender) {
-                hasRecord = true;
-                break;
-            }
+        if (!bookToBorrowers[_bookId][msg.sender]) {
+            bookToArrayOfBorrowers[_bookId].push(address(msg.sender));
+            bookToBorrowers[_bookId][msg.sender] = true;
         }
-        
-        if (!hasRecord) {
-            bookToBorrowers[_bookId].push(address(msg.sender));
-        }
+    }
+    
+    function _generateBookId(string memory _bookName) private view returns (uint) {
+        uint bookId = uint(keccak256(abi.encodePacked(_bookName)));
+        return bookId % bookIdModulus;
+    }
+    
+    function _createBook(string memory _name, uint _copies, uint _id) private bookIsAdded(_id) {
+        books[_id] = Book(_name, _copies, _id);
+        bookIds.push(_id);
     }
 }
